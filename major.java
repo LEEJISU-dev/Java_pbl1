6c93335507750d73499d9b2a237651e97231826c
LEEJISU <amilly0819@gmail.com>
2020-07-09 16:57:42 +0900

    Feat : JAVA Project1 commit

---
+++ major.java
@@ -0,0 +1,238 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class major {
+	
+	public static HashMap<String, major>map = new HashMap<String, major>();
+	
+	String name;
+	String add;
+    String num;
+    String ass;
+    String dean;
+    String email;
+    String url;
+    
+    public major(String name, String add, String num, String ass, String dean, String email, String url){
+    	this.name=name;
+    	this.add=add;
+    	this.num=num;
+    	this.ass=ass;
+    	this.dean=dean;
+    	this.email=email;
+    	this.url=url;
+    }
+    
+    public String toString(){
+        StringBuffer buf = new StringBuffer();
+        buf.append(name).append(" ").append(add).append(" ").append(num).append(" ").append(ass).append(" ")
+        .append(dean).append(" ").append(email).append(" ").append(url).append(" ");
+    	return buf.toString();
+    }
+    
+	public static void ma(){
+		if(map.size()==0){
+			map.put("컴퓨터공학과", new major("컴퓨터공학과","90212","0426297544","변지은","최의인","hnu.kr","http://ce.hannam.ac.kr "));
+			map.put("정보통신공학과", new major("정보통신공학과","90417","0426297567","김수인","류성한","hnu.kr","http://ice.hannam.ac.kr "));
+			map.put("멀티미디어학과", new major("멀티미디어학과","90603","0426298271","이수현","박병주","hnu.kr","http://multimedia.hnu.ac.kr "));
+			map.put("기계공학과", new major("기계공학과","90113","0426298057","이효욱","박문식","hnu.kr","http://me.hannam.ac.kr "));
+			map.put("건축공학과", new major("건축공학과","90509","0426298154","윤지향","김건하","hnu.kr","http://ace.hnu.ac.kr "));
+			map.put("전자공학과", new major("전자공학과","90212","0426297544","김민경","최인식","hnu.kr","http://ee.hannam.ac.kr"));
+			map.put("건축학과", new major("건축학과","60301","0426297996","권미소","신현준","hnu.kr","http://archi.hannam.ac.kr "));
+			map.put("산업경영공학과", new major("산업경영공학과","90403","0426297989","윤다솜","김종수","hnu.kr","http://ime.hannam.ac.kr "));
+			map.put("광센서공학과", new major("광센서공학과","60107","0426297455","신민정","남충희","hnu.kr","http://pns.hannam.ac.kr "));
+			map.put("건설시스템공학과", new major("건설시스템공학과","90104","0426297557","안영은","김건하","hnu.kr","http://civil.hannam.ac.kr "));
+			
+		}
+		
+		System.out.println();
+		System.out.println("[학과 관리] ");
+		System.out.println();
+	    System.out.println("=============================================================================");
+		System.out.println("1. 정보입력 | 2. 정보 삭제 | 3. 정보 확인 및 수정 | 4. 정보 검색 | 5. 전체 학과 확인  | 0. 초기화면");
+		System.out.println("==============================================================================");
+		System.out.println();
+		Scanner scan = new Scanner(System.in);
+        
+		int select = scan.nextInt();
+        switch(select)
+        {
+          case 1 : 
+        	add(); 
+        	break; 
+          case 2 :
+        	del();
+          	break; 
+          case 3 :
+            fix();
+          	break;
+          case 4 : 
+            search();
+          	break;
+          case 5 :
+        	 print();
+          	break; 
+          case 0 :
+          	System.out.println("초기메뉴로 돌아갑니다."); 
+          	info.main(null); 
+        } 
+	}
+
+	public static void add(){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("1. 학과 정보 입력");
+		System.out.println();
+		System.out.println("다음을 순서대로 입력하세요");
+		System.out.println(" 학과명 | 주소 | 전화번호 | 조교명 | 학과장 | 이메일 | 웹사이트 |");
+		System.out.print("학과명 : ");
+		String name = sc.next();
+		System.out.print("주소: ");
+		String add = sc.next();
+		System.out.print("전화번호 : ");
+		int num = sc.nextInt();
+		System.out.print("조교명 : ");
+		String ass = sc.next();
+		System.out.print("학과장 : ");
+		String dean = sc.next();
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		System.out.print("웹사이트 : ");
+		String url = sc.next();
+		
+		System.out.println("입력되었습니다.");
+	
+		System.out.println("======================================"); 
+        System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+        System.out.print(">>");
+        Scanner scanner = new Scanner(System.in);
+        String prin = scanner.nextLine();
+        switch(prin) 
+        {
+        	case "q" : 
+        		ma();
+        		break; 
+  }
+	}
+	
+	public static void del(){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("2. 학과 정보 삭제");
+		System.out.println();
+		System.out.println("삭제할 학과를 입력하세요");
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		map.remove(name);
+		System.out.println(name +"는(은) 삭제되었습니다.");
+	
+		System.out.println("======================================"); 
+        System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+        System.out.print(">>");
+        Scanner scanner = new Scanner(System.in);
+        String prin = scanner.nextLine();
+        switch(prin) 
+        {
+        	case "q" : 
+        		ma();
+        		break; 
+  }
+	}
+	
+	public static void fix(){
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("3. 학과 정보 확인 및 수정");
+		System.out.println();
+		System.out.println("수정할 학과를 입력하세요");
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.println(name +"의 정보입니다.");
+		System.out.println(map.get(name));
+		
+		System.out.println("수정할 데이터를 다음 순서대로 입력하세요");
+		System.out.println();
+		System.out.println(" 주소 | 전화번호 | 조교명 | 학과장 | 이메일 | 웹사이트 |");
+		
+		System.out.print("주소: ");
+		String add = sc.next();
+		System.out.print("전화번호 : ");
+		int num = sc.nextInt();
+		System.out.print("조교명 : ");
+		String ass = sc.next();
+		System.out.print("학과장 : ");
+		String dean = sc.next();
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		System.out.print("웹사이트 : ");
+		String url = sc.next();
+		
+		
+		System.out.println("수정되었습니다.");
+	    
+		System.out.println("======================================"); 
+        System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+        System.out.print(">>");
+        Scanner scanner = new Scanner(System.in);
+        String prin = scanner.nextLine();
+        switch(prin) 
+        {
+        	case "q" : 
+        		ma();
+        		break; 
+  }
+	}
+	public static void search(){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("4. 학과 정보 검색");
+		System.out.println();
+		System.out.println("검색할 학과를 입력하세요");
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.println(name +"의 정보입니다.");
+		System.out.println(map.get(name));
+		
+		System.out.println("======================================"); 
+        System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+        System.out.print(">>");
+        Scanner scanner = new Scanner(System.in);
+        String prin = scanner.nextLine();
+        switch(prin) 
+        {
+        	case "q" : 
+        		ma();
+        		break; 
+  }
+	}
+	public static void print(){
+		
+		System.out.println();
+		System.out.println("5. 전체 학과 확인");
+		System.out.println();
+		for(String key : map.keySet()){
+			System.out.println(map.get(key));
+		}
+		 System.out.println("======================================"); 
+         System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+         System.out.print(">>");
+         Scanner scanner = new Scanner(System.in);
+         String prin = scanner.nextLine();
+         switch(prin) 
+         {
+         	case "q" : 
+         		ma();
+         		break; 
+   }
+	}
+
+
+}
---
+++ professor.java
@@ -0,0 +1,255 @@
+import java.util.Scanner;
+
+import pbl.clear;
+
+import java.util.HashMap;
+
+public class professor extends person{
+	public static HashMap<String, professor>map = new HashMap<String, professor>();
+	public static professor p = new professor();
+
+    String maj;
+    String rank;
+    int date=0;
+    
+    public professor(String name, int id, String num, String email, String les, int pay, String maj, String rank, int date){
+    	super(name, id, num, email, les, pay);//업캐스팅
+    	this.maj=maj;
+    	this.rank=rank;
+    	this.date=date;
+    }
+    
+    public professor(){
+    	
+    }
+    
+
+    
+//오버라이딩
+	public String toString(){
+        StringBuffer buf = new StringBuffer();
+        buf.append(name).append(" ").append(id).append(" ").append(num).append(" ").append(email).append(" ").
+        append(les).append(" ").append(pay).append(" ").append(maj).append(" ").append(rank).append(" ").append(date).append(" ");
+    	return buf.toString();
+    }
+
+	public static void pro(){
+		
+		if(map.size()==0){
+		map.put("최의인", new professor("최의인",12345, "0426297981","eichol@hnu.kr","컴퓨터공학과", 300, "데이터베이스","교수",00000000));
+		map.put("이상구", new professor("이상구",23456, "0426297551","sglee@hnu.kr","컴퓨터공학과", 300, "컴퓨터구조 및 퍼지이론","교수",00000000));
+		map.put("박우전", new professor("박우전",34567, "0426297547","woojunpark@gmail.com","컴퓨터공학과", 300, "프로그래밍언어","교수",00000000));
+		map.put("이강수", new professor("이강수",45678, "0426297549","gslee@hnu.kr","컴퓨터공학과", 300, "소프트웨어공학","교수",00000000));
+		map.put("이극", new professor("이 극",56789, "0426297550","leegeuk@hnu.kr","컴퓨터공학과", 300, "정보보호 및 인공지능","교수",00000000));
+		map.put("이재광", new professor("이재광",67890, "0426297559","jklee@hnu.kr","컴퓨터공학과", 300, "컴퓨터네트워크","교수",00000000));
+		map.put("소우영", new professor("소우영",78901, "0426297657","wsoh@hnu.kr","컴퓨터공학과", 300, "뉴럴네트윅인공지능","교수",00000000));
+		map.put("류성한", new professor("류성한",89012, "0426298531","ktkim@hnu.kr","정보통신공학과", 300, "공학소프트웨어","부교수",00000000));
+		map.put("박대철", new professor("박대철",90123, "0426297571","deachul@hnu.kr","정보통신공학과", 300, "미디어정보처리","교수",00000000));
+		map.put("박성우", new professor("박성우",01234, "0426297398","swpark@hnu.kr","정보통신공학과", 300, "컴퓨터통신망","교수",00000000));
+		}
+		System.out.println();
+		System.out.println("[교수 관리] ");
+		System.out.println();
+	    System.out.println("=============================================================================");
+		System.out.println("1. 정보입력 | 2. 정보 삭제 | 3. 정보 확인 및 수정 | 4. 정보 검색 | 5. 전체 교수 확인  | 0. 초기화면");
+		System.out.println("==============================================================================");
+		System.out.println();
+		Scanner scan = new Scanner(System.in);
+        
+		int select = scan.nextInt();
+        switch(select)
+        {
+          case 1 : 
+        	p.add(); 
+        	break; 
+          case 2 :
+        	p.del();
+          	break; 
+          case 3 :
+            p.fix();
+          	break;
+          case 4 : 
+            p.search();
+          	break;
+          case 5 :
+        	 p.print();
+          	break; 
+          case 0 :
+          	System.out.println("초기메뉴로 돌아갑니다."); 
+          	info.main(null); 
+        } 
+	}
+
+	public void add(){
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println();
+		System.out.println("1. 교수 정보 입력");
+		System.out.println();
+		System.out.println("다음을 순서대로 입력하세요");
+		System.out.println(" 이름 |사번 | 전화번호 | 이메일 | 학과 | 월급 | 강의과목 | 직급 | 입사일 |");
+		System.out.print("이름 : ");
+		String name = sc.next();
+		System.out.print("사번 : ");
+		int id = sc.nextInt();
+		System.out.print("전화번호 : ");
+		String num = sc.next();
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		System.out.print("학과 : ");
+		String les = sc.next();
+		System.out.print("월급 : ");
+		int pay = sc.nextInt();
+		System.out.print("강의 과목 : ");
+		String maj = sc.next();
+		System.out.print("직급: ");
+		String rank = sc.next();
+		System.out.print("입사일 : ");
+		int date = sc.nextInt();
+		
+		map.put(name, new professor(name, id, num, email, les, pay, maj, rank, date));
+		
+		System.out.println("입력되었습니다.");
+		
+		 System.out.println("======================================"); 
+         System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+         System.out.print(">>");
+         Scanner scanner = new Scanner(System.in);
+         String prin = scanner.nextLine();
+         switch(prin) 
+         {
+         	case "q" : 
+         		pro();
+         		break; 
+   }
+		
+	}
+	public void del(){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("2. 교수 정보 삭제");
+		System.out.println();
+		System.out.println("삭제할 교수의 이름을 입력하세요");
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		map.remove(name);
+		System.out.println(name +"교수님의 정보는 삭제되었습니다.");
+		
+		 System.out.println("======================================"); 
+         System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+         System.out.print(">>");
+         Scanner scanner = new Scanner(System.in);
+         String prin = scanner.nextLine();
+         switch(prin) 
+         {
+         	case "q" : 
+         		
+         		pro();
+         		break; 
+   }
+	}
+	public void fix(){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("3. 교수 정보 확인 및 수정 ");
+		System.out.println();
+		System.out.println("확인 및 수정할 교수의 이름을 입력하세요");
+		String name = sc.nextLine();
+		
+		System.out.println(name +"교수님의 정보입니다.");
+		System.out.println(map.get(name));
+
+		
+		System.out.println("수정할 데이터를 다음 순서대로 입력하세요");
+		System.out.println();
+		System.out.println(" 사번 | 전화번호 | 이메일 | 학과 | 월급 | 강의과목 | 직급 | 입사일 |");
+
+		System.out.print("사번 : ");
+		int id = sc.nextInt();
+		System.out.print("전화번호 : ");
+		String num = sc.next();
+		System.out.print("이메일 : ");
+		String email = sc.next();
+		System.out.print("학과 : ");
+		String les = sc.next();
+		System.out.print("월급 : ");
+		int pay = sc.nextInt();
+		System.out.print("강의 과목 : ");
+		String maj = sc.next();
+		System.out.print("직급: ");
+		String rank = sc.next();
+		System.out.print("입사일 : ");
+		int date = sc.nextInt();
+		
+		map.put(name, new professor(name, id, num, email, les, pay, maj, rank, date));
+		
+		System.out.println("수정되었습니다.");
+		
+		 System.out.println("======================================"); 
+         System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+         System.out.print(">>");
+         Scanner scanner = new Scanner(System.in);
+         String prin = scanner.nextLine();
+         switch(prin) 
+         {
+         	case "q" : 
+         	
+         		pro();
+         		break; 
+   }
+	}
+	public   void search(){
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println();
+		System.out.println("4. 교수 정보 검색");
+		System.out.println();
+		System.out.println("검색할 교수의 이름을 입력하세요");
+		System.out.println("이름 : ");
+		String name = sc.nextLine();
+		
+		System.out.println(name +"교수님의 정보입니다.");
+		System.out.println(map.get(name));
+	
+	
+	 System.out.println("======================================"); 
+     System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+     System.out.print(">>");
+     Scanner scanner = new Scanner(System.in);
+     String prin = scanner.nextLine();
+     switch(prin) 
+     {
+     	case "q" : 
+     	
+     		pro();
+     		break; 
+     		}
+	}
+	public   void print(){
+		
+		System.out.println();
+		System.out.println("5. 전체 교수 확인");
+		System.out.println();
+		for(String key : map.keySet()){
+			System.out.println(map.get(key));
+		}
+		
+		 System.out.println("======================================"); 
+         System.out.println("상위메뉴로 돌아가려면 q를 입력하세요.");
+         System.out.print(">>");
+         Scanner scanner = new Scanner(System.in);
+         String prin = scanner.nextLine();
+         switch(prin) 
+         {
+         	case "q" : 
+         		
+         		pro();
+         		break; 
+   }
+	}
+
+}
